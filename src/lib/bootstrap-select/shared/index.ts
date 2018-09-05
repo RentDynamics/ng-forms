ff7f9f0a3d81625c26fb65b184395bbee337b33a
Dave Gines <dgines@rentdynamics.com>
2018-09-05 13:00:31 -0600

    Update exports

--- src/lib/bootstrap-select/shared/index.ts
+++ src/lib/bootstrap-select/shared/index.ts
@@ -2,4 +2,4 @@ export * from './bootstrap-dropdown-menu/index';
 export * from './bootstrap-actions-box/index';
 export * from './bootstrap-toggle-btn/index';
 export * from './bootstrap-filter/index';
-export * from './bootstrap-active-filter';
+export * from './bootstrap-active-filter/index';
